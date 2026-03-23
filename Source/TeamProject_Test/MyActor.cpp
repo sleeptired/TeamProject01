97a9961b016fb3d93f772184dc4c8c48828889a8
sleeptired <107916334+sleeptired@users.noreply.github.com>
2026-03-23 13:24:41 +0900

    [Mine] MyActor 클래스 리팩토링

--- Source/TeamProject_Test/MyActor.cpp
+++ Source/TeamProject_Test/MyActor.cpp
@@ -16,7 +16,7 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!!!!"));
 	
 }
 
@@ -25,7 +25,7 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!~~~~~"));
 
 }
 
